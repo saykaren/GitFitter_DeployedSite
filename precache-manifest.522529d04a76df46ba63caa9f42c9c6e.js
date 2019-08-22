1328b3f781dc129a86929d6160546cc9aa0b53f0
Karen Garcia <karen.garcia@nexscreen.com>
2019-08-22 11:43:29 -0600

    initial repository of build folder with deploy items

---
+++ precache-manifest.522529d04a76df46ba63caa9f42c9c6e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "758c153a167262dd1af2ac7318005023",
+    "url": "/GitFitter_DeployedSite/index.html"
+  },
+  {
+    "revision": "6ae0d15293da83de77c4",
+    "url": "/GitFitter_DeployedSite/static/css/main.36d7a6e8.chunk.css"
+  },
+  {
+    "revision": "60de16a9ff81017a6ae2",
+    "url": "/GitFitter_DeployedSite/static/js/2.73bdaf09.chunk.js"
+  },
+  {
+    "revision": "6ae0d15293da83de77c4",
+    "url": "/GitFitter_DeployedSite/static/js/main.a57026ff.chunk.js"
+  },
+  {
+    "revision": "c6ab4d5b43b189e156e2",
+    "url": "/GitFitter_DeployedSite/static/js/runtime~main.f12e023d.js"
+  },
+  {
+    "revision": "f2aef0dafeb0184e7f5fc21a3bb9c939",
+    "url": "/GitFitter_DeployedSite/static/media/checkMark.f2aef0da.png"
+  },
+  {
+    "revision": "14554d371b37b05f8f6cc2b9a35f9d61",
+    "url": "/GitFitter_DeployedSite/static/media/friends.14554d37.png"
+  },
+  {
+    "revision": "4ee98378ff2cd65d37dc47e7b6cd0023",
+    "url": "/GitFitter_DeployedSite/static/media/sayKaren_Logo_transparent_7.2019.4ee98378.png"
+  },
+  {
+    "revision": "187161009d058d7d853a43552e5a3486",
+    "url": "/GitFitter_DeployedSite/static/media/sleep.18716100.png"
+  },
+  {
+    "revision": "a7c2fd933f42e8cd4d7b17488939999e",
+    "url": "/GitFitter_DeployedSite/static/media/water_glass.a7c2fd93.png"
+  },
+  {
+    "revision": "4dcf98c7b34a4b4504b720f390832562",
+    "url": "/GitFitter_DeployedSite/static/media/welcome.4dcf98c7.png"
+  }
+]);
